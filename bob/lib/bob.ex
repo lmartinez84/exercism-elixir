37c8bd710ec2c08d7d49eae5a8bf762e59611b25
Lisandro Martinez <lisandromartinez@gmail.com>
2020-09-09 19:26:40 -0300

    submit bob

--- bob/lib/bob.ex
+++ bob/lib/bob.ex
@@ -1,7 +1,28 @@
 defmodule Bob do
+  @cyrilic ~r/^.\p{Cyrillic}.*$/u
+  @only_numbers ~r{^[0-9,\s]*$}
+  @question ~r{\?[\s]*$}
+  @say_nothing ~r{^\s*$}
+  @shouting ~r{^([A-Z0-9,\s!($#@*^%])*$}
+  @something_else true
+  @yell_question ~r{^([A-Z\s'!])*\?$}
+
   def hey(input) do
     cond do
-      true -> raise "Your implementation goes here"
+      is_saying_nothing(input) -> "Fine. Be that way!"
+      is_shouting(input) -> "Whoa, chill out!"
+      is_asking_yelling(input) -> "Calm down, I know what I'm doing!"
+      is_asking(input) -> "Sure."
+      @something_else -> "Whatever."
     end
+  end
+
+  defp is_asking(input), do: Regex.match?(@question, input)
+  defp is_asking_yelling(input), do: Regex.match?(@yell_question, input)
+  defp is_saying_nothing(input), do: Regex.match?(@say_nothing, input)
+
+  defp is_shouting(input) do
+    (Regex.match?(@shouting, input) or Regex.match?(@cyrilic, input)) and
+      not Regex.match?(@only_numbers, input)
   end
 end
