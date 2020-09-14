77098a557e4fb41ba1931fccd1d8aa20c943dc27
Lisandro Martinez <lisandromartinez@gmail.com>
2020-09-14 16:33:12 -0300

    submit scrabble

--- scrabble-score/lib/scrabble.ex
+++ scrabble-score/lib/scrabble.ex
@@ -3,6 +3,21 @@ defmodule Scrabble do
   Calculate the scrabble score for the word.
   """
   @spec score(String.t()) :: non_neg_integer
+  def score(""), do: 0
+  def score(" \t\n"), do: 0
+
   def score(word) do
+    word
+    |> String.upcase()
+    |> String.to_charlist()
+    |> Enum.reduce(0, fn char, total -> total + compute(char) end)
   end
+
+  def compute(char) when char in [?A, ?E, ?I, ?O, ?U, ?L, ?N, ?R, ?S, ?T], do: 1
+  def compute(char) when char in [?D, ?G], do: 2
+  def compute(char) when char in [?B, ?C, ?M, ?P], do: 3
+  def compute(char) when char in [?F, ?H, ?V, ?W, ?Y], do: 4
+  def compute(?K), do: 5
+  def compute(char) when char in [?J, ?X], do: 8
+  def compute(char) when char in [?Q, ?Z], do: 10
 end
