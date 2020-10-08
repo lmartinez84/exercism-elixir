affa88b55554c60751f68313907a12e31bc1ceb8
Lisandro Martinez <lisandromartinez@gmail.com>
2020-10-08 18:27:22 -0300

    hamming resubmit

--- hamming/lib/hamming.ex
+++ hamming/lib/hamming.ex
@@ -9,29 +9,28 @@ defmodule Hamming do
   """
   @spec hamming_distance([char], [char]) :: {:ok, non_neg_integer} | {:error, String.t()}
   def hamming_distance(strand1, strand2) do
-    compare_strands(0, strand1, strand2)
+    compare_strands(strand1, strand2, 0)
   end
 
-  defp compare_strands(n, [h1 | t1], [h2 | t2]) do
-    compute_if_different({h1, h2}, n)
-    |> compare_strands(t1, t2)
+  @spec compare_strands([char], [char], non_neg_integer) :: {:ok, non_neg_integer} | {:error, String.t()}
+  defp compare_strands([nucleotide | t1], [nucleotide | t2], n) do
+    compare_strands(t1, t2, n)
   end
 
-  defp compare_strands(n, [], []) do
+  defp compare_strands([_ | t1], [_ | t2], n) do
+    compare_strands(t1, t2, n + 1)
+  end
+
+  defp compare_strands([], [], n) do
     {:ok, n}
   end
 
-  defp compare_strands(_, [], [_ | _]) do
+  defp compare_strands([], [_ | _], _) do
     {:error, "Lists must be the same length"}
   end
 
-  defp compare_strands(_, [_ | _], []) do
+  defp compare_strands([_ | _], [],_) do
     {:error, "Lists must be the same length"}
   end
 
-  defp compute_if_different({nucleotide1, nucleotide2}, n) when nucleotide1 != nucleotide2 do
-    n + 1
-  end
-
-  defp compute_if_different(_, n), do: n
 end
